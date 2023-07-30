59c42cb4dd63194cc8f8535111be4f51eb271f16
Raunak445 <76612926+Raunak445@users.noreply.github.com>
2023-07-30 18:14:55 +0530

    Commit

---
+++ 2020A7PS1699G_CA_Lab5b_PipelinedMIPS/5bitsReg.v
@@ -0,0 +1,16 @@
+/*##########################################################################################
+Note: Please don’t upload the assignments, template file/solution and lab. manual on GitHub or others public repository. 
+Kindly remove them, if you have uploaded the previous assignments. 
+It violates the BITS’s Intellectual Property Rights (IPR).
+*******************************************************************************************/
+//The Dff_Reg will be used for making the pipeline registers
+
+module register5bit(input clk, input reset, input regWrite, input [4:0] inR, output [4:0] outR);
+    //WRITE YOUR CODE HERE, NO NEED TO DEFINE NEW VARIABLES
+    genvar i;
+    generate
+        for (i = 0; i < 5; i = i + 1) 
+            Dff_Reg d(clk,reset,regWrite,inR[i],outR[i]);
+    endgenerate
+
+endmodule
---
+++ 2020A7PS1699G_CA_Lab6a_PipelinedHDU_MIPS/adder.v
@@ -0,0 +1,6 @@
+module adder(input [31:0] in1, input [31:0] in2, output reg [31:0] adderOut);
+    always@(in1, in2)
+    begin
+        adderOut = in1 + in2;
+    end
+endmodule
